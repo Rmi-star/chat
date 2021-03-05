8dc75011f30a4bd3a45f79d553f7b79b7fd47528
Rmi_star <dkfma3323@naver.com>
2021-03-05 13:56:41 +0900

    Initial commit

---
+++ src/main/java/com/kar/chat/ChatingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kar.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatingProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kar/chat/ChatingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kar.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
